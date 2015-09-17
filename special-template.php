b54959fbc7a4fbc4f71031b42553ad87bba41fd1
rimapro <rima.voskanyan@gmail.com>
2015-09-17 22:37:24 +0400

    lessons for WordPress adding all files to github

---
+++ special-template.php
@@ -0,0 +1,30 @@
+﻿<?php
+/* 
+Template Name: Special layout
+*/
+
+get_header();
+
+if(have_posts()):
+	while (have_posts()): the_post(); ?>
+	
+	<article class="post page">
+	<h2><?php the_title();?></h2>
+	<!-- info-box -->
+	<div class="info-box">
+		<h4>Disclaimer title</h4>
+		<p> hiuydawjbdjhagwqwandemngjgdaf </p>
+	</div> <!-- /info-box -->
+	
+	<?php the_content();?>
+	</article>
+	
+	<?php 
+	endwhile;
+	
+	else:
+	echo '<p>no content found</p>';
+	
+	endif;
+get_footer();
+?>
